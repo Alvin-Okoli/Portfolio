74c8dae3f0caae146dd4d9e96089b9d92d33539b
Alvin <alvinokoli@gmail.com>
2025-05-29 21:16:13 +0100

    added Afrilog blog

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -8,49 +8,61 @@ export default function Project({topic, project}){
                 <div className="text-orange-500">Projects</div>
             </div>
 
-            <a href="https://shopa-delta.vercel.app/" target="_blank">
-                <div className="mx-2 py-4 rounded-2xl my-6 grid grid-cols-3 gap-x-2 hover:bg-gray-100 cursor-pointer md:gap-x-5">
+            <div>
+                <div className="mx-2 py-4 rounded-2xl my-6 grid grid-cols-3 gap-x-2 hover:bg-gray-100 cursor-pointer md:gap-x-5 relative">
                 <img src="Screenshot 2025-05-05 160356.png" alt="" className=""/>
                 <div className="col-span-2">
                     <div className="font-bold text-xl md:text-2xl">Shopa</div>
                     <p className="text-gray-700 md:text-xl my-2">A full-stack e-commerce site</p>
                     <p className="text-gray-700 md:text-xl my-2 hidden lg:block"> Discover our curated selection of products and categories at Shopa. Fast shipping, secure checkout, and top-notch customer service!Showcasing my latest project: a MERN stack e-commerce site! 💻 Built with React, Node.js, Express, and MongoDB. Code quality and performance were top priorities...</p>
+                    <a href="https://shopa-delta.vercel.app/" target="_blank">
+                        <div className="absolute bottom-1 animate-pulse font-bold hover:underline hover:text-orange-500">Click to preview</div>
+                    </a>
                 </div>
             </div>
-            </a>
+            </div>
 
-            <a href="https://shopa-delta.vercel.app/" target="_blank">
-                <div className="mx-2 py-4 rounded-2xl my-6 grid grid-cols-3 gap-x-2 hover:bg-gray-100 cursor-pointer md:gap-x-5">
-                <img src="Screenshot 2025-05-05 160356.png" alt="" className=""/>
+            <div>
+                <div className="mx-2 py-4 rounded-2xl my-6 grid grid-cols-3 gap-x-2 hover:bg-gray-100 cursor-pointer md:gap-x-5 relative">
+                <img src="Screenshot 2025-05-29 203603.png" alt="" className=""/>
                 <div className="col-span-2">
-                    <div className="font-bold text-xl md:text-2xl">Shopa</div>
-                    <p className="text-gray-700 md:text-xl my-2">A full-stack e-commerce site</p>
-                    <p className="text-gray-700 md:text-xl my-2 hidden lg:block"> Discover our curated selection of products and categories at Shopa. Fast shipping, secure checkout, and top-notch customer service!Showcasing my latest project: a MERN stack e-commerce site! 💻 Built with React, Node.js, Express, and MongoDB. Code quality and performance were top priorities...</p>
+                    <div className="font-bold text-xl md:text-2xl">Afrolog</div>
+                    <p className="text-gray-700 md:text-xl my-2">A full-stack blog.</p>
+                    <p className="text-gray-700 md:text-xl my-2 hidden lg:block">A fully functional blog site built using the MERN (MongoDB, Express.js, React.js, Node.js) stack! This blog site showcases my skills in building scalable and efficient web applications using the MERN stack. I'm proud to share this project with you, and I hope you enjoy exploring it...</p>
+                    <a href="https://mern-blog-one-ruby.vercel.app/" target="_blank">
+                        <div className="absolute bottom-1 animate-pulse font-bold hover:underline hover:text-orange-500">Click to preview</div>
+                    </a>
                 </div>
             </div>
-            </a>
+            </div>
 
-            <a href="https://shopa-delta.vercel.app/" target="_blank">
-                <div className="mx-2 py-4 rounded-2xl my-6 grid grid-cols-3 gap-x-2 hover:bg-gray-100 cursor-pointer md:gap-x-5">
+            <div href="https://shopa-delta.vercel.app/" target="_blank">
+                <div className="mx-2 py-4 rounded-2xl my-6 grid grid-cols-3 gap-x-2 hover:bg-gray-100 cursor-pointer md:gap-x-5 relative">
                 <img src="Screenshot 2025-05-05 160356.png" alt="" className=""/>
                 <div className="col-span-2">
                     <div className="font-bold text-xl md:text-2xl">Shopa</div>
                     <p className="text-gray-700 md:text-xl my-2">A full-stack e-commerce site</p>
                     <p className="text-gray-700 md:text-xl my-2 hidden lg:block"> Discover our curated selection of products and categories at Shopa. Fast shipping, secure checkout, and top-notch customer service!Showcasing my latest project: a MERN stack e-commerce site! 💻 Built with React, Node.js, Express, and MongoDB. Code quality and performance were top priorities...</p>
+                    <a href="https://shopa-delta.vercel.app/" target="_blank">
+                        <div className="absolute bottom-1 animate-pulse font-bold hover:underline hover:text-orange-500">Click to preview</div>
+                    </a>
                 </div>
             </div>
-            </a>
+            </div>
             
-            <a href="https://shopa-delta.vercel.app/" target="_blank">
-                <div className="mx-2 py-4 rounded-2xl my-6 grid grid-cols-3 gap-x-2 hover:bg-gray-100 cursor-pointer md:gap-x-5">
+            <div href="https://shopa-delta.vercel.app/" target="_blank">
+                <div className="mx-2 py-4 rounded-2xl my-6 grid grid-cols-3 gap-x-2 hover:bg-gray-100 cursor-pointer md:gap-x-5 relative">
                 <img src="Screenshot 2025-05-05 160356.png" alt="" className=""/>
                 <div className="col-span-2">
                     <div className="font-bold text-xl md:text-2xl">Shopa</div>
                     <p className="text-gray-700 md:text-xl my-2">A full-stack e-commerce site</p>
                     <p className="text-gray-700 md:text-xl my-2 hidden lg:block"> Discover our curated selection of products and categories at Shopa. Fast shipping, secure checkout, and top-notch customer service!Showcasing my latest project: a MERN stack e-commerce site! 💻 Built with React, Node.js, Express, and MongoDB. Code quality and performance were top priorities...</p>
+                    <a href="https://shopa-delta.vercel.app/" target="_blank">
+                        <div className="absolute bottom-1 animate-pulse font-bold hover:underline hover:text-orange-500">Click to preview</div>
+                    </a>
                 </div>
             </div>
-            </a>
+            </div>
         </div>
     )
 }
