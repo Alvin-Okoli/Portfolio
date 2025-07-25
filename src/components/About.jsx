8a2268b77bb67b2b0fed26a99706cda33d8f10d0
Alvin <alvinokoli@gmail.com>
2025-07-25 14:29:20 +0100

    added projects, info in contact section and changed hardcodes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,58 +1,103 @@
+import { useState } from "react";
+
+const workExperiences = [
+    {
+        duration: "May 2025 - present",
+        role: "Software Engineer",
+        company: "Halofy Africa 🎨",
+        description: "Currently working with the Halofy team on a full-stack project that combines my passion for stunning UI/UX, scalable architecture, and optimized databases. This project features a responsive and intuitive frontend built with React and tailwindcss, ensuring a seamless user experience across devices. The backend is powered by Node.js and Express, providing a robust and scalable architecture to handle high traffic and complex data interactions. The database is designed with MongoDB, allowing for efficient data management and retrieval. ",
+        responsibilities: [
+        "Responsive design for optimal user experience on all devices.",
+        "Scalable architecture to handle high traffic and complex data interactions.",
+        "Optimized database for efficient data management and retrieval.",
+        "Intuitive UI/UX design for enhanced user engagement.",
+        "Integration of modern web technologies for a seamless experience.",
+        ],
+        technologies: ["React", "Tailwind CSS", "Node.js", "Express", "MongoDB"],
+    },
+    {
+        duration: "2024-present",
+        role: "Freelance Web Designer",
+        company: "Devin Corp 🪙",
+        description: "Freelance web development projects for clients via the Devin platform.",
+        responsibilities: [
+        "Worked independently on a variety of web development projects for clients, delivering full-stack solutions with a focus on clean code, performance, and user experience.",
+        "Built and maintained websites and web applications using modern JavaScript frameworks and libraries, including React, Node.js, and Express.",
+        "Collaborated with clients to gather requirements, provide technical consultation, and implement custom features that aligned with their business goals.",
+        "Integrated third-party APIs, optimized applications for SEO and speed, and ensured mobile responsiveness across devices.",
+        "Managed end-to-end deployment, testing, and debugging, delivering high-quality software on time and within scope.",
+        "Gained hands-on experience in freelancing, client communication, and agile project delivery over the course of a year. This has honed my skills in full-stack development, project management, and client relations, preparing me for future challenges in the tech industry. ",
+        ],
+    },
+    ];
 
 export default function About(){
+    const [clicked, setClicked] = useState(false);
+
+        async function copyToClipboard(text) {
+            try {
+            await navigator.clipboard.writeText(text);
+            }
+            catch (err) {
+            console.error('Failed to copy: ', err);
+            }
+        }
 
     return(
         <div className="px-2 md:px-6 lg:pl-24 lg:pr-48">
-            <div className="text-5xl md:text-7xl font-bold my-6">About <span className="text-orange-500">Me</span></div>
+            <div className="text-5xl md:text-7xl font-bold my-6 mb-10">About <span className="text-orange-500">Me</span></div>
 
-            <div className="px-4">
-                <div className="font-bold md:text-2xl md:my-2">Technologies I use:</div>
-                <li><span className="font-semibold md:text-xl">Frontend: </span> React & tailwindcss. </li>
-                <li><span className="font-semibold md:text-xl">Backend: </span> Node.js and express. </li>
-                <li><span className="font-semibold md:text-xl">Game developement: </span> Phaser.</li>
-                <li><span className="font-semibold md:text-xl">Database: </span> MongoDB.</li>
+            <div className="px-2">
+                <div className="text-2xl md:text-5xl font-bold my-6">Technologies I use:</div>
+                {skills.map((skill, index) => (
+                    <li key={index}><span className="font-semibold md:text-xl">{skill.category}: </span> {skill.tools.join(', ')} </li>
+                ))}
             </div>
 
             <div className="text-2xl md:text-5xl font-bold my-6">Experience</div>
 
-            <div className="border-b-2 py-7 border-gray-500 mb-4">
-                <div className="my-2 font-semibold md:text-2xl">May 2025 - present</div>
-                <div className="font-semibold md:text-2xl">Software engineer</div>
-                <div className="font-semibold md:text-2xl">Halofy Africa 🎨</div>
-                <div className="text-gray-500 my-2 md:text-xl md:my-6">
-                    Currently working with the Halofy team on a full-stack project that combines my passion for stunning UI/UX, scalable architecture, and optimized databases. This project features a responsive and intuitive frontend built with React and tailwindcss, ensuring a seamless user experience across devices. The backend is powered by Node.js and Express, providing a robust and scalable architecture to handle high traffic and complex data interactions. The database is designed with MongoDB, allowing for efficient data management and retrieval.
-                    Key features include:
-                    <ul className="list-disc ml-6 mt-2">
-                        <li>Responsive design for optimal user experience on all devices.</li>
-                        <li>Scalable architecture to handle high traffic and complex data interactions.</li>
-                        <li>Optimized database for efficient data management and retrieval.</li>
-                        <li>Intuitive UI/UX design for enhanced user engagement.</li>
-                        <li>Integration of modern web technologies for a seamless experience.</li>
-                    </ul>
-                </div>
+            { workExperiences.map((experience, index) =>
+                <div className="border-b-2 py-7 border-gray-500 mb-4 lg:">
+                    <div className="my-2 font-semibold md:text-2xl">{experience.duration}</div>
+                    <div className="font-semibold md:text-2xl">{experience.role}</div>
+                    <div className="font-semibold md:text-2xl">{experience.company}</div>
+                    <div className="text-gray-500 my-2 md:text-xl md:my-6">
+                        {experience.description}
+                        Key features include:
+                        <ul className="list-disc ml-6 mt-2">
+                            {experience.responsibilities.map((feature, featureIndex) => (
+                                <li key={featureIndex}>{feature}</li>
+                            ))}
+                        </ul>
+                    </div>
             </div>
+            )}
 
-            <div className="border-b-2 py-7 border-gray-500 my-4">
-                <div className="my-2 font-semibold md:text-2xl">2024-present</div>
-                <div className="font-semibold md:text-2xl">Freelance web designer</div>
-                <div className="font-semibold md:text-2xl">Devin Corp</div>
-                <div className="text-gray-500 my-2 md:text-xl md:my-6">
-                    I have been a freelancer at Devin:
-                    <li>Worked independently on a variety of web development projects for clients via the Devin platform, delivering full-stack solutions with a focus on clean code, performance, and user experience.</li>
+            <a href="Okoli_Alvin_Resume.pdf" download="Okoli_Alvin_Resume.pdf" className="my-2 hover:text-orange-500 underline text-center">Download Resume</a>
 
-                    <li>Built and maintained websites and web applications using modern JavaScript frameworks and libraries, including React, Node.js, and Express.</li>
-
-                    <li>Collaborated with clients to gather requirements, provide technical consultation, and implement custom features that aligned with their business goals.</li>
-
-                    <li>Integrated third-party APIs, optimized applications for SEO and speed, and ensured mobile responsiveness across devices.</li>
-
-                    <li>Managed end-to-end deployment, testing, and debugging, delivering high-quality software on time and within scope.</li>
-
-                    <li>Gained hands-on experience in freelancing, client communication, and agile project delivery over the course of a year.</li>
-                </div>
-            </div>
-
-            <a href="Okoli_Alvin_Resume.pdf" download="Okoli_Alvin_Resume.pdf" className="my-2 hover:text-orange-500 underline">Download Resume</a>
         </div>
     )
 }
+
+const skills = [
+        {
+            category: "Frontend",
+            tools: ["React", "Tailwind CSS"],
+        },
+        {
+            category: "Backend",
+            tools: ["Node.js", "Express"],
+        },
+        {
+            category: "Game Development",
+            tools: ["Phaser"],
+        },
+        {
+            category: "Database",
+            tools: ["MongoDB"],
+        },
+        {
+            category: "Tools",
+            tools: ["Git", "GitHub", 'Postman', 'Docker', 'WebSockets', 'RESTful APIs'],
+        }
+    ];
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,5 +1,42 @@
 
-export default function Project({topic, project}){
+export default function Project(){
+    const projects = [
+  {
+    title: "Shopa",
+    description: "A full-stack e-commerce site.",
+    longDescription: "Discover our curated selection of products and categories at Shopa. Fast shipping, secure checkout, and top-notch customer service! Showcasing my latest project: a MERN stack e-commerce site!💻 Built with React, Node.js, Express, and MongoDB. Code quality and performance were top priorities.",
+    image: "Screenshot 2025-05-05 160356.png",
+    link: "https://shopa-delta.vercel.app/",
+  },
+  {
+    title: "Afrolog",
+    description: "A full-stack blog that speaks about Africa.",
+    longDescription: "A fully functional blog site built using the MERN (MongoDB, Express.js, React.js, Node.js) stack! This blog site showcases my skills in building scalable and efficient web applications using the MERN stack.",
+    image: "Screenshot 2025-05-29 203603.png",
+    link: "https://mern-blog-one-ruby.vercel.app/",
+  },
+  {
+    title: "Law Firm Website",
+    description: "A comprehensive website for law firms, featuring attorney profiles and practice areas.",
+    longDescription: "This law firm website is designed to provide a robust online presence for law firms, featuring attorney profiles, practice areas, and a blog for sharing legal insights. Built with a focus on credibility and trust, this website features a clean design, easy navigation, and a responsive layout.",
+    image: "Screenshot 2025-07-25 014222.png",
+    link: "https://uche-ebrim-co.vercel.app/",
+  },
+  {
+    title: "World Countries API",
+    description: "This was a challenge from Frontend Mentors to test the use of API and also the developer's UI/UX skills.",
+    longDescription: "This project is a solution to the Frontend Mentor challenge, designed to test API integration and UI/UX skills. It fetches and displays country data from a REST API, featuring RESTful API, image optimization, and theme switch.",
+    image: "desktop-preview.jpg",
+    link: "https://country-rest-api-black.vercel.app/",
+  },
+  {
+    title: "Fun Pong",
+    description: "A fun single 2D arcade pong game.",
+    longDescription: "This game is a classic arcade-style game built using Phaser 3. This game features simple yet addictive gameplay, where you try to keep the ball from hitting the bottom corner of the screen, you get 3 lives and each hit of the ball is a point.",
+    image: "Screenshot 2025-05-27 153442.png",
+    link: "https://fun-pong.vercel.app/",
+  }
+];
 
     return(
         <div className="md:px-6 py-6 lg:pl-24 lg:pr-48">
@@ -9,83 +46,27 @@ export default function Project({topic, project}){
             </div>
 
             <div>
-                <div className="mx-2 py-4 rounded-2xl my-6 grid grid-cols-3 gap-x-2 hover:bg-gray-100 shadow md:gap-x-5">
-                    <a href="Screenshot 2025-05-05 160356.png" target="_blank"><img src="Screenshot 2025-05-05 160356.png" alt="" className="max-h-60 cursor-pointer"/></a>
-                    <div className="col-span-2">
-                        <a href="https://shopa-delta.vercel.app/" target="_blank">
-                            <div className="font-bold text-xl md:text-2xl">Shopa</div>
+                {projects.map((project, index) => (
+                    <div key={index} className="mx-2 py-4 rounded-2xl my-6 grid grid-cols-3 gap-x-2 hover:bg-gray-100 shadow md:gap-x-5 relative">
+                        <a href={project.image} target="_blank">
+                            <img src={project.image} alt="" className="max-h-60 cursor-pointer"/>
                         </a>
-                        <p className="text-gray-700 md:text-xl my-2">A full-stack e-commerce site.</p>
-                        <p className="text-gray-700 my-2 px-2 hidden md:text-xl lg:block"> Discover our curated selection of products and categories at Shopa. Fast shipping, secure checkout, and top-notch customer service!Showcasing my latest project: a MERN stack e-commerce site! 💻 Built with React, Node.js, Express, and MongoDB. Code quality and performance were top priorities...</p>
-                        <a href="https://shopa-delta.vercel.app/" target="_blank">
-                            <div className="absolute bottom-1 animate-pulse font-bold hover:underline hover:text-orange-500 cursor-pointer">Click to preview</div>
-                        </a>
-                    </div>
-                </div>
-            </div>
 
-            <div>
-                <div className="mx-2 py-4 rounded-2xl my-6 grid grid-cols-3 gap-x-2 hover:bg-gray-100 shadow md:gap-x-5">
-                    <a href="Screenshot 2025-05-29 203603.png" target="_blank"><img src="Screenshot 2025-05-29 203603.png" alt="" className="max-h-60 cursor-pointer"/></a>
-                    <div className="col-span-2">
-                    <a href="https://mern-blog-one-ruby.vercel.app/" target="_blank">
-                        <div className="font-bold text-xl md:text-2xl">Afrolog</div>
-                    </a>
-                        <p className="text-gray-700 md:text-xl my-2">A full-stack blog that speaks about Africa.</p>
-                        <p className="text-gray-700 my-2 px-2 hidden md:text-xl lg:block">A fully functional blog site built using the MERN (MongoDB, Express.js, React.js, Node.js) stack! This blog site showcases my skills in building scalable and efficient web applications using the MERN stack. I'm proud to share this project with you, and I hope you enjoy exploring it...</p>
-                        <a href="https://mern-blog-one-ruby.vercel.app/" target="_blank">
-                            <div className="absolute bottom-1 animate-pulse font-bold hover:underline hover:text-orange-500 cursor-pointer">Click to preview</div>
-                        </a>
-                    </div>
-                </div>
-            </div>
+                        <div className="col-span-2 lg:px-2">
+                            <a href={project.link} target="_blank">
+                                <div className="font-bold text-xl md:text-2xl">{project.title}</div>
+                            </a>
 
-            <div>
-                <div className="mx-2 py-4 rounded-2xl my-6 grid grid-cols-3 gap-x-2 hover:bg-gray-100 shadow md:gap-x-5">
-                    <a href="Screenshot 2025-05-05 160356.png" target="_blank"><img src="Screenshot 2025-05-05 160356.png" alt="" className="max-h-60 cursor-pointer"/></a>
-                    <div className="col-span-2">
-                        <a href="https://shopa-delta.vercel.app/" target="_blank">
-                            <div className="font-bold text-xl md:text-2xl">Shopa</div>
-                        </a>
-                        <p className="text-gray-700 md:text-xl my-2">A full-stack e-commerce site</p>
-                        <p className="text-gray-700 my-2 px-2 hidden md:text-xl lg:block"> Discover our curated selection of products and categories at Shopa. Fast shipping, secure checkout, and top-notch customer service!Showcasing my latest project: a MERN stack e-commerce site! 💻 Built with React, Node.js, Express, and MongoDB. Code quality and performance were top priorities...</p>
-                        <a href="https://shopa-delta.vercel.app/" target="_blank">
-                            <div className="absolute bottom-1 animate-pulse font-bold hover:underline hover:text-orange-500 cursor-pointer">Click to preview</div>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            
-            <div>
-                <div className="mx-2 py-4 rounded-2xl my-6 grid grid-cols-3 gap-x-2 hover:bg-gray-100 shadow md:gap-x-5">
-                    <a href="Screenshot 2025-05-27 153442.png" target="_blank"><img src="Screenshot 2025-05-27 153442.png" alt="" className="max-h-60 cursor-pointer"/></a>
-                    <div className="col-span-2">
-                        <a href="https://fun-pong.vercel.app/" target="_blank">
-                            <div className="font-bold text-xl md:text-2xl">Fun Pong</div>
-                        </a>
-                        <p className="text-gray-700 md:text-xl my-2">A fun single 2d arcade pong game.</p>
-                        <p className="text-gray-700 my-2 px-2 hidden md:text-xl lg:block"> This game is a classic arcade-style game built using Phaser 3. This game features simple yet addictive gameplay, where you try to kp th ball from hitting the bottom corner of the screen, you get 3 lives and each hit of the ball is a ponit. I hope you enjoy playing my Pong game!</p>
-                        <a href="https://fun-pong.vercel.app/" target="_blank">
-                            <div className="absolute bottom-1 animate-pulse font-bold hover:underline hover:text-orange-500 cursor-pointer">Click to preview</div>
-                        </a>
-                    </div>
-                </div>
-            </div>
+                            <p className="text-gray-700 md:text-xl my-2">{project.description}</p>
 
-            <div>
-                <div className="mx-2 py-4 rounded-2xl my-6 grid grid-cols-3 gap-x-2 hover:bg-gray-100 shadow md:gap-x-5">
-                    <a href="desktop-preview.jpg" target="_blank"><img src="desktop-preview.jpg" alt="" className="max-h-60 cursor-pointer"/></a>
-                    <div className="col-span-2">
-                        <a href="https://country-rest-api-black.vercel.app/" target="_blank">
-                            <div className="font-bold text-xl md:text-2xl">World Countries API.</div>
-                        </a>
-                        <p className="text-gray-700 md:text-xl my-2">This was a challenge from frontendmentors to test the use of API and also the developers ui/ux skills.</p>
-                        <p className="text-gray-700 my-2 px-2 hidden md:text-xl lg:block"> This project is a solution to the Frontend Mentor challenge, designed to test API integration and UI/UX skills. It fetches and displays country data from a REST API, featuring RESTful API, image optimization and theme switch.</p>
-                        <a href="https://country-rest-api-black.vercel.app/" target="_blank">
-                            <div className="absolute bottom-1 animate-pulse font-bold hover:underline hover:text-orange-500 cursor-pointer">Click to preview</div>
-                        </a>
+                            <p className="text-gray-700 my-2 hidden md:text-xl lg:block"> {project.longDescription}..</p>
+
+                            <a href={project.link} target="_blank">
+                                <div className="absolute bottom-1 animate-pulse font-bold hover:underline hover:text-orange-500 cursor-pointer">Click to preview</div>
+                            </a>
+                        </div>
                     </div>
-                </div>
+                ))}
             </div>
         </div>
     )
